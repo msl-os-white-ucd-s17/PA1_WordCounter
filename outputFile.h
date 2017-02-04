cbe14d7b769a8af7da68245a2fd2bc56c042b78d
SaraKim529 <sara.kim@ucdenver.edu>
2017-02-03 17:26:23 -0700

    Update and rename menu.h to outputFile.h

---
+++ outputFile.h
@@ -0,0 +1,15 @@
+//
+// Created by Stefani on 1/31/2017.
+// Edited to be writeFile not menu
+
+#ifndef BST_FILEOUT_H
+#define BST_FILEOUT_H
+
+#include <stdio.h>
+
+#include "bst.h"
+
+void writeFile(Node*);
+char* convertToLower(char*);
+
+#endif //BST_FILEOUT_H
